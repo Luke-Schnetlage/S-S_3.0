fb9f2aa78b20475349cdb68147a74b00d26fb587
Luke Schnetlage <89532838+Luke-Schnetlage@users.noreply.github.com>
2023-03-27 22:25:25 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+//library for routing functionality (Lets pages load w/o refreshing)
+import { Route, Routes } from "react-router-dom"
+
+//CSS
+import './styles/fonts.css';
+import './styles/main.css'
+//relative HTML
+import Navbar from "./static_content/Navbar"
+import Game from "./pages/Game"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import Rules from "./pages/Rules"
+import Footer from "./static_content/Footer"
+
+function App() {
+  return (
+    <div>
+    <Navbar />
+    <div id="page-container">
+      <div id="content-wrap">
+        <Routes>
+          <Route path="/" element={ <Home />} />
+          <Route path="/game" element={ <Game />} />
+          <Route path="/login" element={ <Login />} />
+          <Route path="/rules" element={ <Rules />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
+   </div>
+  );
+}
+
+export default App;
